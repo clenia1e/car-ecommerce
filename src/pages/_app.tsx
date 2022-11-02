74ed7160a51e1ec722ce638219a7b107f8eb3f51
clenia1e <cleniaguedes1@gmail.com>
2022-11-01 22:00:08 -0300

    fix: fixed and left responsive card list

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Home from "../components/Home/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Home />
+      {/* <Component {...pageProps} />; */}
+    </div>
+  );
 }
 
 export default MyApp;
